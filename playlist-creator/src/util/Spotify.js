c845800c828ab6bc8ea326525c3b566546eb5ea3
helenavprado <helena.vprado@hotmail.com>
2022-10-20 00:02:50 -0700

    cant update state after api request

--- playlist-creator/src/util/Spotify.js
+++ playlist-creator/src/util/Spotify.js
@@ -1,5 +1,6 @@
 const clientId = "e3e9ca1af934479c8b0a86dece349eed";
 const redirectURI = "http://localhost:3000/";
+let accessToken;
 
 /*
 request
@@ -9,8 +10,7 @@ const query = "?type=track&q=TERM";
 const urlToFetch = baseURL + endPoint + query;
 */
 
-let accessToken;
-let Spotify = {
+const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken;
@@ -20,7 +20,7 @@ let Spotify = {
       window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if (userUrlToken && expiresIn) {
+    if (userUrlTokenMatch && expiresInMatch) {
       accessToken = userUrlTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
       window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
@@ -32,30 +32,53 @@ let Spotify = {
     }
   },
 
+  // search(userInput) {
+  //   const accessToken = Spotify.getAccessToken();
+  //   const searchh = async (userInput) => {
+  //     try {
+  //       const response = await fetch(
+  //         `https://api.spotify.com/v1/search?type=track&q=${userInput}`,
+  //         { headers: { Authorization: `Bearer ${accessToken}` } }
+  //       );
+  //       if (response.ok) {
+  //         const jsonResponse = await response.json();
+  //         return jsonResponse.tracks.items.map((track) => ({
+  //           id: track.id,
+  //           name: track.name,
+  //           artist: track.artists[0].name,
+  //           album: track.album.name,
+  //           uri: track.uri,
+  //         }));
+  //       }
+
+  //       throw new Error("request failed");
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  // },
+
   search(userInput) {
+    console.log(`spotify.search user input ${userInput}`);
     const accessToken = Spotify.getAccessToken();
-    fetch(`https://api.spotify.com/v1/search?type=track&q=${userInput}`, {
+    fetch(`https://api.spotify.com/v1/search?q=${userInput}&type=track`, {
       headers: { Authorization: `Bearer ${accessToken}` },
     })
       .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
+        return response.json();
       })
       .then((jsonResponse) => {
         if (!jsonResponse.tracks) {
           return [];
-        } else {
-          return jsonResponse.tracks.map((track) => {
-            return {
-              id: track.id,
-              name: track.name,
-              artist: track.artists[0].name,
-              album: track.album.name,
-              uri: track.uri,
-            };
-          });
         }
+        console.log(jsonResponse.tracks);
+        return jsonResponse.tracks.items.map((track) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          album: track.album.name,
+          uri: track.uri,
+        }));
       });
   },
 };
